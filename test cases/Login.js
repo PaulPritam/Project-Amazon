469f9d36f4d401055385cc85391fba82ddef0925
PaulPritam <pritampaul9797@gmail.com>
2024-08-11 21:48:08 +0530

    MODIFIED: Optimized the code

--- test cases/Login.js
+++ test cases/Login.js
@@ -1,8 +1,26 @@
-Feature('Login functionalities check');
+// const assert = require('assert');
+const { expect } = require('chai');
+const { assert } = require('chai');
 
-Scenario('Test 1: Check login functionalities' , ({ I }) =>
-{
-    console.log("Connected.........................")
-I.moveCursorTo("//span[text() = 'Account & Lists']")
+Feature('Launch Browser and Navigate');
 
+Before(({ I }) => {
+    I.amOnPage('https://www.amazon.in/');
+});
+
+Scenario('Test 1: Check login functionalities', async ({ I }) => {
+    console.log("Hovering over sign in ")
+    const accountListBtn = locate("//span[text() = 'Account & Lists']");
+    I.moveCursorTo(accountListBtn);
+    const signInBtn = locate("//div[@id='nav-flyout-ya-signin']//a//child::span[text() = 'Sign in']");
+    I.waitForClickable(signInBtn);
+    I.click(signInBtn);
+    console.log("Clicked on sign in button, awaiting navigation to login page and continue button to be displayed");
+
+    const continueBtn = locate("//input[@id='continue']")
+    I.waitForClickable(continueBtn);
+    const actualSignInText = await I.grabTextFrom("//h1[contains(text(), 'Sign in')]");
+    console.log(".." + actualSignInText + "..");
+
+    expect(actualSignInText).to.include('Sign in');
 });
